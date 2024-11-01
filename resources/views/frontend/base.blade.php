71c4a32447dded6235ac0ef94d3f348f2d4035f4
Kinan <kinan@holooltech.com>
2024-11-01 03:04:51 +0300

    third

--- resources/views/frontend/base.blade.php
+++ resources/views/frontend/base.blade.php
@@ -6,14 +6,14 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <title>Demo Business Consulting 2 | Porto - Multipurpose Website Template</title>
+    <title>EuroLegal UK</title>
 
     <meta name="keywords" content="WebSite Template" />
     <meta name="description" content="Porto - Multipurpose Website Template">
     <meta name="author" content="okler.net">
 
     <!-- Favicon -->
-    <link rel="shortcut icon" href="{{asset('frontend/img/favicon.ico')}}" type="image/x-icon" />
+    <link rel="shortcut icon" href="{{asset('frontend/images/title-logo.png')}}" type="image/x-icon" />
     <link rel="apple-touch-icon" href="{{asset('frontend/img/apple-touch-icon.png')}}">
 
     <!-- Mobile Metas -->
